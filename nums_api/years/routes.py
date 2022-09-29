b668c35381d976b99c9750ebba2f97ff2d587a59
Billy Russell <billy.hrussell@gmail.com>
2022-09-29 14:10:07 -0700

    added routes for Years routes include fact for single year and fact for random year

--- nums_api/years/routes.py
+++ nums_api/years/routes.py
@@ -1,21 +1,71 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from nums_api.years.models import Year
+from sqlalchemy.sql import func
 
 years = Blueprint("years", __name__)
 
 
-@years.get("/<year>")
+@years.get("/<int:year>")
 def get_year_fact(year):
-    """FIXME
-    Stub route for getting fact about year.
+    """ Route for getting fact about year.
+
+    If fact found, returns JSON like:
+        {
+            "type": year,
+            "id" : 1,
+            "year" : 1,
+            "fact_fragment": "text",
+            "fact_statement": "text",
+            }
+
+    If no fact found, returns error message like:
+    { error:
+        { "message": "A test fact for year test not found",
+          "status": 404
+        }
+    }
     """
 
-    return f"Some interesting fact about the year {year}."
+    year_data = Year.query.filter_by(year = year).order_by(func.random()).first()
+
+    if year_data:
+        year_fact = {
+            'type': 'year',
+            'id' : year_data.id,
+            'year': year_data.year,
+            'fact_fragment': year_data.fact_fragment,
+            'fact_statement': year_data.fact_statement
+        }
+
+        return jsonify(fact = year_fact), 200
+    else:
+        err = { 'error' : {
+            'message': f"A fact for { year } not found",
+        }}
+        return jsonify(err), 404
+
 
 
 @years.get("/random")
 def get_year_fact_random():
-    """FIXME
-    Stub route for getting fact about random year.
+    """ Route for getting fact about random year.
+        Returns JSON like:
+            {
+            "type": year,
+            "id" : 1,
+            "year" : 1,
+            "fact_fragment": "text",
+            "fact_statement": "text",
+            }
     """
+    year_data = Year.query.order_by(func.random()).first()
 
-    return "Some interesting fact about a random year."
+    year_fact = {
+        'type': 'year',
+        'id' : year_data.id,
+        'year': year_data.year,
+        'fact_fragment': year_data.fact_fragment,
+        'fact_statement': year_data.fact_statement
+    }
+
+    return jsonify(fact = year_fact), 200
