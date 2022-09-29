8146d4e6c442d5cbf07d7462ea06905837784cc4
Billy Russell <billy.hrussell@gmail.com>
2022-09-29 16:14:04 -0700

    Created test files for Year routes tests for get_year_fact and get_year_fact_random

--- nums_api/years/routes.py
+++ nums_api/years/routes.py
@@ -1,22 +1,24 @@
 from flask import Blueprint, jsonify
 from nums_api.years.models import Year
 from sqlalchemy.sql import func
+import random
 
 years = Blueprint("years", __name__)
 
 
 @years.get("/<int:year>")
 def get_year_fact(year):
-    """ Route for getting fact about year.
+    """ Route for getting a random fact about year.
 
     If fact found, returns JSON like:
-        {
+        {fact: {
             "type": year,
             "id" : 1,
             "year" : 1,
             "fact_fragment": "text",
             "fact_statement": "text",
             }
+        }
 
     If no fact found, returns error message like:
     { error:
@@ -26,46 +28,49 @@ def get_year_fact(year):
     }
     """
 
-    year_data = Year.query.filter_by(year = year).order_by(func.random()).first()
+    year_data = Year.query.filter_by(year = year).all()
 
     if year_data:
+        single_year_data = random.choice(year_data)
         year_fact = {
             'type': 'year',
-            'id' : year_data.id,
-            'year': year_data.year,
-            'fact_fragment': year_data.fact_fragment,
-            'fact_statement': year_data.fact_statement
+            'id' : single_year_data.id,
+            'year': single_year_data.year,
+            'fact_fragment': single_year_data.fact_fragment,
+            'fact_statement': single_year_data.fact_statement
         }
 
-        return (jsonify(fact = year_fact), 200)
+        return jsonify(fact = year_fact)
     else:
         err = {
+            'status': 404,
             'message': f"A fact for { year } not found",
         }
-        return (jsonify(error=err), 404)
-
-
+        return (jsonify(error = err), 404)
 
 @years.get("/random")
 def get_year_fact_random():
     """ Route for getting fact about random year.
         Returns JSON like:
-            {
+        { fact: {
             "type": year,
             "id" : 1,
             "year" : 1,
             "fact_fragment": "text",
             "fact_statement": "text",
-            }
+            }}
     """
-    year_data = Year.query.order_by(func.random()).first()
+    # year_data = Year.query.order_by(func.random()).first()
+    year_data = Year.query.all()
+
+    single_year_data = random.choice(year_data)
 
     year_fact = {
         'type': 'year',
-        'id' : year_data.id,
-        'year': year_data.year,
-        'fact_fragment': year_data.fact_fragment,
-        'fact_statement': year_data.fact_statement
+        'id' : single_year_data.id,
+        'year': single_year_data.year,
+        'fact_fragment': single_year_data.fact_fragment,
+        'fact_statement': single_year_data.fact_statement
     }
 
-    return (jsonify(fact = year_fact), 200)
+    return jsonify(fact = year_fact)
--- nums_api/years/tests/test_routes.py
+++ nums_api/years/tests/test_routes.py
@@ -16,35 +16,30 @@ class YearRouteTestCase(TestCase):
     def setUp(self):
         """Set up test data here"""
         self.client = app.test_client()
-        
+
         Year.query.delete()
-        
-        y1 = Year(
+
+        self.y1 = Year(
             year=1,
             fact_fragment="the year for the first test entry",
             fact_statement="First test for year 1",
             was_submitted=False
         )
-        y2 = Year(
+        self.y2 = Year(
             year=2,
             fact_fragment="the year for the second test entry",
             fact_statement="2 is the year for this test fact statement.",
             was_submitted=False
         )
-        y3 = Year(
+        self.y3 = Year(
             year=1,
             fact_fragment="the year for the third test entry",
             fact_statement="Another test for year 1.",
             was_submitted=False
         )
 
-        db.session.add_all([y1,y2,y3])
+        db.session.add_all([self.y1, self.y2, self.y3])
         db.session.commit()
-        
-        self.y1_yr = y1.year
-        self.y2_yr = y2.year
-        self.y3_yr = y3.year
-        
 
     def tearDown(self):
         """Clean up any fouled transaction."""
@@ -54,19 +49,40 @@ class YearRouteTestCase(TestCase):
         """Test to make sure tests are set up correctly"""
         test_setup_correct = True
         self.assertEqual(test_setup_correct, True)
-    
-    def test_single_fact_route(self):
-        """Tests for getting a single fact by year."""
-        with self.client as c:
-    
-            resp = c.get("/api/years/1")
-            self.assertEqual(resp.status_code, 200)
-            print('response object', resp)
-            # self.assertIn("year 1", resp)
 
-            #Test failure on non-existant year:
-            # resp = c.get("/years/100000")
-            
-            # self.assertEqual(resp.status_code, 404)
-            # self.assertIn("not found", resp.error)
-            
+    def test_get_year_fact(self):
+        """Tests for getting a single fact by year.
+        Get a single fact about the year 1"""
+        with self.client as c:
+
+            resp = c.get("/api/years/1")
+            print("FACT ", resp.json["fact"])
+            self.assertIn("year 1", resp.json["fact"]["fact_statement"])
+            self.assertEqual(resp.status_code, 200)
+
+    def test_error_for_year_without_fact(self):
+        """Tests for getting an error for a year without a fact.
+        """
+        with self.client as c:
+
+            resp = c.get("api/years/100000")
+            self.assertIn("not found", resp.json["error"]["message"])
+            self.assertEqual(resp.status_code, 404)
+
+    def test_get_year_fact_random(self):
+        """Test for getting a single fact on a random year."""
+        with self.client as c:
+
+            resp = c.get("api/years/random")
+            self.assertTrue(resp.json["fact"]["fact_fragment"])
+            with self.assertRaises(KeyError):
+                resp.json["error"]["message"]
+            self.assertEqual(resp.status_code, 200)
+
+    def test_invalid_year_url_input(self):
+        """Test for invalid URL input for a year."""
+        with self.client as c:
+
+            resp = c.get("api/years/BLAH")
+            self.assertFalse(resp.json)
+            self.assertEqual(resp.status_code, 404)
