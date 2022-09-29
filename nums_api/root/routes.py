5f8dee87da2fc7a1edb882dfc89f37abd890315a
Phill Legnitto <palegnitto@gmail.com>
2022-09-29 14:53:53 -0700

    implimented python markdown for reading a file and converting to html

--- nums_api/root/routes.py
+++ nums_api/root/routes.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, render_template
+from jinja_markdown import MarkdownExtension
+import markdown
+
+
+
 
 root = Blueprint("root", __name__, template_folder='templates')
 
@@ -11,4 +16,13 @@ def homepage():
 
     """
 
-    return render_template('base.html')
+    with open("nums_api/documentation.md", "r", encoding="utf-8") as file:
+        text = file.read()
+
+    html = markdown.markdown(text,output_format="xhtml", extensions=["fenced_code"])
+
+    # md_template_string = markdown.markdown('## URL Structure')
+    # print(md_template_string)
+    # md = Markdown(md)
+
+    return render_template('home.html', documentation=html)
