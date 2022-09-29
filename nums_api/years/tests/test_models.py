801ae5bae70324c37220dfea746e18aa1a3196de
Edmond Soun <edmond.soun@gmail.com>
2022-09-29 10:55:21 -0700

    adds tests for year model

--- nums_api/years/tests/test_models.py
+++ nums_api/years/tests/test_models.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from nums_api import app
 from nums_api.database import db
 from nums_api.config import DATABASE_URL_TEST
+from nums_api.years.models import Year
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL_TEST
 app.config["TESTING"] = True
@@ -14,6 +15,15 @@ db.create_all()
 class YearModelTestCase(TestCase):
     def setUp(self):
         """Set up test data here"""
+        
+        Year.query.delete()
+
+        self.y1 = Year(
+            year=2000,
+            fact_fragment="the year all the computers crashed",
+            fact_statement="2000 is the year all the computers crashed.",
+            was_submitted=False
+        )
 
     def tearDown(self):
         """Clean up any fouled transaction."""
@@ -23,3 +33,14 @@ class YearModelTestCase(TestCase):
         """Test to make sure tests are set up correctly"""
         test_setup_correct = True
         self.assertEqual(test_setup_correct, True)
+        
+
+    def test_model(self):
+        self.assertIsInstance(self.y1, Year)
+        self.assertEqual(Year.query.count(), 0)
+
+        db.session.add(self.y1)
+        db.session.commit()
+
+        self.assertEqual(Year.query.count(), 1)
+        self.assertEqual(Year.query.filter_by(year=2000).one().year, 2000)
