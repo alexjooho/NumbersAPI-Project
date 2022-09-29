5683e1e9b256623ad8cb82b303c2a923d433be1c
Francis Carino <fgcarino@gmail.com>
2022-09-29 15:42:42 -0700

    favicon add favicon route to routes.py

--- nums_api/root/routes.py
+++ nums_api/root/routes.py
@@ -1,28 +1,29 @@
-from flask import Blueprint, render_template
-from jinja_markdown import MarkdownExtension
+from flask import Blueprint, render_template, send_from_directory
+import os
 import markdown
 
 
-
-
-root = Blueprint("root", __name__, template_folder='templates')
+root = Blueprint("root", __name__, template_folder='templates',
+                 static_folder='static')
 
 
 @root.get("/")
 def homepage():
     """Show homepage:
-
         - Renders API docs from a markdown file to Jinja Templates.
-
     """
 
     with open("nums_api/documentation.md", "r", encoding="utf-8") as file:
         text = file.read()
 
-    html = markdown.markdown(text,output_format="xhtml", extensions=["fenced_code"])
-
-    # md_template_string = markdown.markdown('## URL Structure')
-    # print(md_template_string)
-    # md = Markdown(md)
+    html = markdown.markdown(text, output_format="xhtml",
+                             extensions=["fenced_code"])
 
     return render_template('home.html', documentation=html)
+
+
+@root.route('/favicon.ico')
+def favicon():
+    """Display favicon, saving the extra redirect request"""
+    return send_from_directory(os.path.join(root.root_path, 'static'),
+                               'favicon.ico', mimetype='static/favicon.ico')
