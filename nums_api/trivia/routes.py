0d929855133ad84aff6c7505a1d1209ca6ec09eb
Alex <alexjooho@gmail.com>
2022-09-29 12:17:21 -0700

    worked on issue 3
    wrote routes for getting a trivia fact for a specific number and for getting a random fact
    also wrote tests for these two routes

--- nums_api/trivia/routes.py
+++ nums_api/trivia/routes.py
@@ -1,21 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from nums_api.trivia.models import Trivia
+from random import choice
 
 trivia = Blueprint("trivia", __name__)
 
 
 @trivia.get("/<number>")
 def get_trivia_fact(number):
-    """FIXME
-    Stub route for getting trivia fact about number.
     """
+    Route for getting trivia fact about a number.
+    Returns json { number, fact } if number is valid, else will return error:
+    { error: { message: f"A { category } fact for { number } not found", status: 404} }
+    """
+    
+    trivia = Trivia.query.filter_by(number=number).first()
+    
+    if not trivia:
+        error = {
+            'error': {
+                'message': f"A trivia fact for { number } not found",
+                'status': 404
+            }
+        }
+        
+        return jsonify(error), 404
 
-    return f"Some interesting trivia fact about {number}."
+    return jsonify(number = number, fact = trivia.fact_statement)
 
 
 @trivia.get("/random")
 def get_trivia_fact_random():
-    """FIXME
-    Stub route for getting trivia fact about random number.
     """
+    Route for getting random trivia fact.
+    Returns json { random: fact }
+    """
+    
+    random_trivia = choice(Trivia.query.all())
+    
+    # session.query(Trivia).order_by(func.rand()).first()
 
-    return "Some interesting trivia fact about a random number."
+    return jsonify( random = random_trivia.fact_fragment)
--- nums_api/trivia/tests/test_routes.py
+++ nums_api/trivia/tests/test_routes.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from nums_api import app
 from nums_api.database import db
 from nums_api.config import DATABASE_URL_TEST
+from nums_api.trivia.models import Trivia
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL_TEST
 app.config["TESTING"] = True
@@ -14,6 +15,36 @@ db.create_all()
 class TriviaRouteTestCase(TestCase):
     def setUp(self):
         """Set up test data here"""
+        Trivia.query.delete()
+        
+        t1 = Trivia(
+            number=1,
+            fact_fragment='cool number',
+            fact_statement='1 is the first number after 0.',
+            was_submitted=True
+        )
+        
+        t2 = Trivia(
+            number=2,
+            fact_fragment='not a cool number',
+            fact_statement='2 is the most lame number.',
+            was_submitted=True
+        )
+        
+        t3 = Trivia(
+            number=3,
+            fact_fragment='meh number',
+            fact_statement='3 is honestly a meh number.',
+            was_submitted=True
+        )
+        
+        db.session.add_all([t1, t2, t3])
+        db.session.commit()
+        
+        self.t1_id = t1.id
+        self.t2_id = t2.id
+        self.t3_id = t3.id
+        
         self.client = app.test_client()
 
     def tearDown(self):
@@ -24,3 +55,50 @@ class TriviaRouteTestCase(TestCase):
         """Test to make sure tests are set up correctly"""
         test_setup_correct = True
         self.assertEqual(test_setup_correct, True)
+        
+    def test_specific_number(self):
+        """ Tests for getting a fact about a specific number """
+        with self.client as c:
+            resp = c.get("/api/trivia/1")
+            
+            self.assertEqual(resp.status_code, 200)
+            
+            data = resp.json
+            
+            self.assertEqual(data, {
+                "number": '1',
+                "fact": "1 is the first number after 0."
+            })
+            
+    def test_specific_number_invalid(self):
+        """ Tests for an invalid number """
+        with self.client as c:
+            resp = c.get("/api/trivia/5")
+            
+            self.assertEqual(resp.status_code, 404)
+            
+            data = resp.json
+            
+            self.assertEqual(data, {
+                "error": {
+                    "message": "A trivia fact for 5 not found",
+                    "status": 404
+                }
+            })
+            
+    def test_random(self):
+        """ Tests getting a random fact """
+        with self.client as c:
+            resp = c.get("/api/trivia/random")
+            
+            self.assertEqual(resp.status_code, 200)
+            
+            data = resp.json
+            
+            possible_results = [
+                { "random": "cool number"},
+                { "random": "not a cool number"},
+                { "random": "meh number"}
+            ]
+            
+            self.assertIn(data, possible_results)
