db7d11329edee40835fad2e1b6f36d088aa1c092
Zhenhua <zhenhua1109@yahoo.com>
2022-09-29 11:12:03 -0700

    get_math_fact route
    wip of route returning number fact

--- nums_api/maths/routes.py
+++ nums_api/maths/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from nums_api.maths.routes import Math
 
 math = Blueprint("math", __name__)
 
@@ -8,8 +10,18 @@ def get_math_fact(number):
     """FIXME
     Stub route for getting math fact about number.
     """
+    numData = Math.query.filter_by(number = number).one_or_none()
 
-    return f"Some interesting math fact about {number}."
+    if numData:
+        # structure
+        return jsonify(numData, 200)
+    else:
+        error = { 'error': {
+                    'message': f"A number fact for { number } not found",
+                    'status': 404 } }
+        return (jsonify(error), 404)
+
+    # discuss JSON structure above ^
 
 
 @math.get("/random")
