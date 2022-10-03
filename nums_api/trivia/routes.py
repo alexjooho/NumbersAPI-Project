b87521587c044e9cb0f3684f24720a3f720710be
Alex Kim <107967375+alexjooho@users.noreply.github.com>
2022-10-03 14:16:42 -0700

    3-trivia-route-single (#31)
    * worked on issue 3
    
    wrote routes for getting a trivia fact for a specific number and for getting a random fact
    also wrote tests for these two routes
    
    * revisions for pull request
    
    updated routes.py and test_routes.py
    
    * second revision
    
    * Third review of changes needed to be made.
    
    Formatted dictionaries to be more readable and added tests for invalid number.
    
    * issue 3 completed
    
    minor change in test_routes
    
    Co-authored-by: Bryan Pitman <pitman.bryan@gmail.com>

--- nums_api/trivia/routes.py
+++ nums_api/trivia/routes.py
@@ -1,21 +1,59 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from nums_api.trivia.models import Trivia
+from random import choice
 
 trivia = Blueprint("trivia", __name__)
 
 
-@trivia.get("/<number>")
+@trivia.get("/<int:number>")
 def get_trivia_fact(number):
-    """FIXME
-    Stub route for getting trivia fact about number.
+    """
+    Route for getting trivia fact about a number. The number is received from
+    the url parameter.
+    If the URL parameter is not a positive integer, it will return a 404 error.
+
+    If url parameter is valid, returns json:
+     { fact: { number, fragment, statement, type } }
+
+    Otherwise it returns an error:
+    { error: { message: f"A { category } fact for { number } not found", status: 404} }
     """
 
-    return f"Some interesting trivia fact about {number}."
+    trivia = Trivia.query.filter_by(number=number).all()
+    if not trivia:
+        error = {
+            "status": 404,
+            "message": f"A trivia fact for { number } not found"
+        }
 
+        return jsonify(error=error), 404
+
+    random_trivia = choice(trivia)
+    fact = {
+        "number": random_trivia.number,
+        "fragment": random_trivia.fact_fragment,
+        "statement": random_trivia.fact_statement,
+        "type": "trivia"
+    }
+
+    return jsonify(fact=fact)
 
 @trivia.get("/random")
 def get_trivia_fact_random():
-    """FIXME
-    Stub route for getting trivia fact about random number.
+    """
+    Route for getting random trivia fact.
+    Queries all rows in trivia table and randomly picks one
+    Returns json:
+    { fact: { number, fragment, statement, type } }
     """
 
-    return "Some interesting trivia fact about a random number."
+    random_trivia = choice(Trivia.query.all())
+
+    fact = {
+        "number": random_trivia.number,
+        "fragment": random_trivia.fact_fragment,
+        "statement": random_trivia.fact_statement,
+        "type": "trivia"
+    }
+
+    return jsonify(fact=fact)
