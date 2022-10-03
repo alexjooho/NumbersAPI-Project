6d7972489bd3b55cb86635323f129bb9c13e59ed
Emily Chen <sticky_sugar@hotmail.com>
2022-10-03 12:32:15 -0700

    minor fixes after merging with main
    update random routes to query all
    update json return format to match existing routes
    start on testing error paths

--- nums_api/dates/routes.py
+++ nums_api/dates/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from nums_api.dates.models import Date
-from random import choice, randint
+from random import random, choice, randint
 
 dates = Blueprint("dates", __name__)
 
@@ -9,76 +9,70 @@ dates = Blueprint("dates", __name__)
 def get_date_fact(month, day):
     """route to retreive a random date fact on specified month/day
         returns json
-        {
-        'month': 12,
-        'day': 30,
-        'year': 1783,
-        'fact_fragment': 'some fact frament',
-        'fact_statement': 'some fact statement',
-        'type': 'date'
+        {fact: {
+            'day_of_year': 120,
+            'year': 1783,
+            'fact_fragment': 'some fact frament',
+            'fact_statement': 'some fact statement',
+            'type': 'date'
+            }
         }
-        returns error if month/day not in range
+        returns value error if month/day not in range
+        returns 404 error if no fact for specified month/day
         """
 
     error_msg = {
-        "message": f"A date fact for { month/day } not found",
+        "message": f"A date fact for { month }/{ day } not found",
         "status": 404
     }
 
     try:
         days = Date.date_to_day_of_year(month, day)
     except ValueError as e:
-        return e
+        return (jsonify(error={
+            "message": str(e),
+            "status": 404
+        }), 404)
 
     try:
-        date_fact_res = Date.query.filter_by(day_of_year=days).one()
-        print(date_fact_res)
-
+        res = Date.query.filter_by(day_of_year=days).all()
+        date_fact_res = choice(res)
     except (IndexError):
         return (jsonify(error=error_msg), 404)
 
     date_fact = {
-        'month': month,
-        'day': day,
+        'day_of_year': date_fact_res.day_of_year,
         'year': date_fact_res.year,
         'fact_fragment': date_fact_res.fact_fragment,
         'fact_statement': date_fact_res.fact_statement,
         'type': 'date'
     }
 
-    return jsonify(date_fact)
+    return jsonify(fact=date_fact)
 
 
 @dates.get("/random")
 def get_date_fact_random():
     """route to retreive a random date fact
     returns json
-    {
-    'month': 12,
-    'day': 30,
-    'year': 1783,
-    'fact_fragment': 'some fact frament',
-    'fact_statement': 'some fact statement',
-    'type': 'date'
+    {fact: {
+        'day_of_year': 120,
+        'year': 1783,
+        'fact_fragment': 'some fact frament',
+        'fact_statement': 'some fact statement',
+        'type': 'date'
+        }
     }
     """
 
-    random_fact = None
-
-    # keep querying until there is a random fact
-    while not random_fact:
-        month = randint(1, 12)
-        day = randint(1, months_days[month])
-        days = Date.date_to_day_of_year(month, day)
-        random_fact = choice(Date.query.filter_by(day_of_year=days))
+    date_facts_res = Date.query.all()
+    random_fact = choice(date_facts_res)
 
     random_date_fact = {
-        'month': month,
-        'day': day,
-        'year': random_fact.year,
+        'day_of_year': random_fact.day_of_year,
         'fact_fragment': random_fact.fact_fragment,
         'fact_statement': random_fact.fact_statement,
         'type': 'date'
     }
 
-    return jsonify(random_date_fact)
+    return jsonify(fact=random_date_fact)
--- nums_api/dates/tests/test_routes.py
+++ nums_api/dates/tests/test_routes.py
@@ -52,7 +52,7 @@ class DateRouteTestCase(TestCase):
         self.assertEqual(test_setup_correct, True)
 
     def test_date_fact(self):
-        """Test to retrieve random fact by specific month/day"""
+        """Test route to retrieve random fact by specific month/day"""
         with self.client as c:
             resp = c.get("/api/dates/1/10")
 
@@ -60,11 +60,25 @@ class DateRouteTestCase(TestCase):
 
             data = resp.json
 
-            self.assertEqual(data, ({
-                'month': 1,
-                'day': 10,
-                'year': 1900,
-                'fact_fragment': "test1 fragment",
-                'fact_statement': "test1 statement",
-                'type': 'date'
-            }))
+            self.assertEqual(data, ({"fact":
+                                     {
+                                         'day_of_year': 10,
+                                         'year': 1900,
+                                         'fact_fragment': "test1 fragment",
+                                         'fact_statement': "test1 statement",
+                                         'type': 'date'
+                                     }}))
+
+    def test_invalid_dates(self):
+        """Test route for invalid dates"""
+        with self.client as c:
+            resp = c.get("/api/dates/10/32")
+
+            self.assertEqual(resp.status_code, 404)
+
+            data = resp.json
+
+            self.assertEqual(data, {"error": {
+                "message": "Invalid value for day",
+                "status": 404
+            }})
