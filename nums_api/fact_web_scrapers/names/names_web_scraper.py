5ab887b464de4ba7ab0566a5964fa1c0445fd662
Edmond Soun <edmond.soun@gmail.com>
2022-10-04 14:03:18 -0700

    adds link discovery from main name list

--- nums_api/fact_web_scrapers/names/names_web_scraper.py
+++ nums_api/fact_web_scrapers/names/names_web_scraper.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.babycenter.com/baby-names/details/liam-2820"
-# URL = "https://www.babycenter.com/babyNamerSearch.htm?batchSize=1000"
+# URL = "https://www.babycenter.com/baby-names/details/liam-2820"
+#TODO: make number larger after tests
+URL = "https://www.babycenter.com/babyNamerSearch.htm?batchSize=1000"
 page = requests.get(URL)
 
-print(page.text)
+#CONSOLE PRINT FOR TESTING:
+#print(page.text)
 
-# scrape list page for URLs to specific names
-# loop over name URLs and scrape facts, if present (default if no description is present?)
-# write fact dictionary to text file
+soup = BeautifulSoup(page.content, "html.parser")
+
+# print(soup.prettify())
+
+# # scrape list page for URLs to specific names
+# # loop over name URLs and scrape facts, if present (default if no description is present?)
+# # write fact dictionary to text file
+
+results = soup.find_all(class_="nameCell")
+
+for link in results:
+    print(link.find("a").get("href"))
+    
+# print(results)
