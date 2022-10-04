8a1c3a6a4133c80af028ee35d221d057de4b2c1b
Edmond Soun <edmond.soun@gmail.com>
2022-10-04 16:18:10 -0700

    adds multi-page scraping and file writing functionaliy

--- nums_api/fact_web_scrapers/names/names_web_scraper.py
+++ nums_api/fact_web_scrapers/names/names_web_scraper.py
@@ -2,17 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.behindthename.com/names/sort/alpha/display/details"
-
-
-# for link in results:
-
-#     print(f'{BASE_URL}{link.find("a").get("href")}')
-
-# print(results)
+MAX_PAGE_COUNT = 84
+name_fact_dict = {}
 
 def parse_a_page(url):
+    """Scrapes a page and creates key/value pairs in dictionary:
+    {'Name':'This is a fact', 'Name2':'Another fact.', ...}
+    """
     page = requests.get(url)
-
 
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -22,14 +19,20 @@ def parse_a_page(url):
     for span in soup.find_all(class_="listusage"):
         span.decompose()
 
-    # scrape list page for URLs to specific names
-    # loop over name URLs and scrape facts, if present (default if no description is present?)
-    # write fact dictionary to text file
-
     results = soup.find_all(class_="browsename")
 
+    # Grab key name, then delete containing tag so only fact text remains.
+    # Write key/value pair to dictionary:
     for name in results:
-        print(name.get_text())
+        key = name.find(class_="listname").get_text()
+        name.find(class_="listname").decompose()
+        name_fact_dict[key] = name.get_text().strip()
         
-for pagenumber in range(1,84):
+# Loops over all pages in source website:
+for pagenumber in range(1,MAX_PAGE_COUNT):
     parse_a_page(f"{URL}/{pagenumber}")
+    
+# Writes completed dictionary to text file:
+with open('scraped_names_facts.txt','w') as data: 
+      data.write(str(name_fact_dict))
+
