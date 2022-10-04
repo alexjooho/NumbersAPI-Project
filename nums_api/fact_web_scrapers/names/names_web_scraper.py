84b974d743bde7f9d0db0f7ca0b318bddd468c5c
Edmond Soun <edmond.soun@gmail.com>
2022-10-04 13:33:38 -0700

    adds notes for structure of scraper script

--- nums_api/fact_web_scrapers/names/names_web_scraper.py
+++ nums_api/fact_web_scrapers/names/names_web_scraper.py
@@ -6,3 +6,7 @@ URL = "https://www.babycenter.com/baby-names/details/liam-2820"
 page = requests.get(URL)
 
 print(page.text)
+
+# scrape list page for URLs to specific names
+# loop over name URLs and scrape facts, if present (default if no description is present?)
+# write fact dictionary to text file
