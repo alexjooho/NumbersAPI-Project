b5e25a57e95f342c027d00f41ec99ada7684c496
Edmond Soun <edmond.soun@gmail.com>
2022-10-06 16:06:25 -0700

    removes before_request hook

--- nums_api/__init__.py
+++ nums_api/__init__.py
@@ -18,43 +18,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 
-# request tracking
-
-# @app.before_request
-# def track_request():
-#     """Track request item and category, update total number of requests in DB."""
-    
-#     PATH_SPLIT_INDEX = 5
-#     VALID_CATEGORIES = ["dates", "trivia", "years", "names", "math"]
-
-#     #ensure route is parsable, otherwise return:
-#     try:
-#         [category, req_item] = request.path.lower()[PATH_SPLIT_INDEX:].split("/",1)
-#     except ValueError:
-#         return
-    
-#     #ignore random
-#     if req_item == "random":
-#         return
-    
-#     #ignore requests to non-valid category routes
-#     if category not in VALID_CATEGORIES:
-#         return
-    
-#     if category == "dates":
-#         [month, day] = req_item.split("/")
-#         req_item = Date.date_to_day_of_year(int(month), int(day))
-    
-#     if category == "math":
-#         #check that value is integer
-        
-#     Tracking.update_request_count(req_item, category)
-    
-#     #EDGE CASES NOT CURRENTLY HANDLED:
-#         # no URL authentication: /api/math/banana
-#         # batch requests not excluded
-    
-
 # register blueprints
 app.register_blueprint(root)
 app.register_blueprint(trivia, url_prefix='/api/trivia')
@@ -62,6 +25,7 @@ app.register_blueprint(math, url_prefix='/api/math')
 app.register_blueprint(dates, url_prefix='/api/dates')
 app.register_blueprint(years, url_prefix='/api/years')
 
+# request tracking
 @app.after_request
 def track_request(response):
     """Track request item and category, update total number of requests in DB."""
@@ -95,7 +59,6 @@ def track_request(response):
 
     return response
     
-
 
 # allow CORS and connect app to database
 CORS(app)
