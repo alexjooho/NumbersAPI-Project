b530bf3f255a8d02f99e825cad2d6bb9d425741c
Francis Carino <fgcarino@gmail.com>
2022-10-11 16:37:00 -0700

    build slack fact request function

--- nums_api/slack_fact.py
+++ nums_api/slack_fact.py
@@ -1,12 +1,17 @@
 import requests
 import json
+from random import choice
+
 # put here your webhook_url obtained from the steps above
 webhook_url = "https://hooks.slack.com/services/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"
 
+route = choice(["dates", "trivia", "math", "years"])
+
+fact_resp = requests.get(f"http://localhost:5001/api/{route}/random").json()
+
+statement = fact_resp["fact"]["statement"]
+
 payload = {
-    "channel": "#random",
-    "username": "I AM A BOT",
-    "text": "This is test message.",
-    "icon_emoji": ":monkey:"
+    "text": f"{statement}",
 }
 requests.post(webhook_url, data=json.dumps(payload))
