a35d90e4e3b695ae4501bfa41e0a81339bd2518f
Francis Carino <fgcarino@gmail.com>
2022-10-11 16:39:53 -0700

    add requests to requirements.txt

--- nums_api/slack_fact.py
+++ nums_api/slack_fact.py
@@ -2,12 +2,14 @@ import requests
 import json
 from random import choice
 
+BASE_URL = "http://localhost:5001/api/"
+
 # put here your webhook_url obtained from the steps above
 webhook_url = "https://hooks.slack.com/services/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"
 
 route = choice(["dates", "trivia", "math", "years"])
 
-fact_resp = requests.get(f"http://localhost:5001/api/{route}/random").json()
+fact_resp = requests.get(f"{BASE_URL}{route}/random").json()
 
 statement = fact_resp["fact"]["statement"]
 
