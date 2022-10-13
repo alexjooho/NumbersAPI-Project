f5c6a23b50588a3a863712cd930a3af5fedd9977
Edmond Soun <edmond.soun@gmail.com>
2022-10-13 10:53:05 -0700

    adds math_likes table to seed file

--- nums_api/seed.py
+++ nums_api/seed.py
@@ -1,6 +1,6 @@
 from nums_api import app
 from nums_api.database import db
-from nums_api.maths.models import Math
+from nums_api.maths.models import Math, MathLike
 from nums_api.trivia.models import Trivia
 from nums_api.years.models import Year
 from nums_api.dates.models import Date
