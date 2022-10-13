db1222822c4674ef0c9ccb3f924166057f54fb49
Ryan Rustyn <ryanrustyn@gmail.com>
2022-10-13 10:30:34 -0700

    Add new class for text search vectors

--- nums_api/database.py
+++ nums_api/database.py
@@ -1,4 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import TSVECTOR
+
+class TSVector(sa.types.TypeDecorator):
+    impl = TSVECTOR
 
 db = SQLAlchemy()
 
