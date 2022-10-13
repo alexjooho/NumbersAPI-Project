9b8ed3ec79a5a6e8ee1a5e872e09e64cf7d20723
Ryan Rustyn <ryanrustyn@gmail.com>
2022-10-13 12:26:43 -0700

    add search for all in use dbs
    add search query for all in use databases displaying all results
    add helper function for setting data into JSONable format
    helper function needs work for displaying number for facts

--- nums_api/search/routes.py
+++ nums_api/search/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify, request
+from nums_api.dates.models import Date
+from nums_api.maths.models import Math
 from nums_api.trivia.models import Trivia
+from nums_api.years.models import Year
 
 search = Blueprint("search", __name__)
 
@@ -14,13 +17,29 @@ def get_search_results():
     term = args["query"]
     print("search term:", term)
 
-    results = Trivia.query.filter(Trivia.__ts_vector__.match(term)).all()
-    print("results are:", results)
+    dates_results = Date.query.filter(Date.__ts_vector__.match(term)).all()
+    math_results = Math.query.filter(Math.__ts_vector__.match(term)).all()
+    trivia_results = Trivia.query.filter(Trivia.__ts_vector__.match(term)).all()
+    years_results = Year.query.filter(Year.__ts_vector__.match(term)).all()
 
-    results = [{"number": r.number,
-                "statement": r.fact_statement,
-                "type": "trivia"
-                }
-               for r in results]
+    dates_results = create_results_array(dates_results, "date")
+    math_results = create_results_array(math_results, "math")
+    trivia_results = create_results_array(trivia_results, "trivia")
+    years_results = create_results_array(years_results, "years")
+    
+    results = {
+        "dates": dates_results,
+        "math": math_results,
+        "trivia": trivia_results,
+        "years": years_results
+    }
 
     return jsonify(results=results)
+
+
+def create_results_array(results, oftype):
+    """Create a List from results of search query"""
+    return [{"statement": r.fact_statement,
+             "type": oftype
+             }
+            for r in results]
