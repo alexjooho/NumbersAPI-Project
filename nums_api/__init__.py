f31fe57ea0d164be92068f075223e181927c254c
Billy Russell <billy.hrussell@gmail.com>
2022-10-13 12:21:10 -0700

    Cleaned up code and removed ngrok

--- nums_api/__init__.py
+++ nums_api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 from nums_api.config import DATABASE_URL
+from nums_api.config import RANDOM_TRIVIA_FACT_URL
 from nums_api.database import connect_db
 from nums_api.trivia.routes import trivia
 from nums_api.maths.routes import math
@@ -10,23 +11,16 @@ from nums_api.years.routes import years
 from nums_api.root.routes import root
 from nums_api.tracking.models import Tracking
 from nums_api.dates.models import Date
-from flask_ngrok2 import run_with_ngrok
+
 
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
-
-# Replace URL with production/live URL
-RANDOM_TRIVIA_FACT_URL = "http://localhost:5002/api/trivia/random"
 
 # create app and add configuration
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-run_with_ngrok(app)
-
-if __name__ == "__main__":
-    app.run()
 
 # register blueprints
 app.register_blueprint(root)
@@ -79,29 +73,17 @@ def incoming_sms():
     # Get the message the user sent our Twilio number
     txt = request.values.get('Body').lower()
 
-    # When database is updated, use below line to get fact. When website is live,
-    # replace RANDOM_TRIVIA_FACT_URL with production/live URL for random trivia fact
-    # response = requests.get(RANDOM_TRIVIA_FACT_URL)
-    # When database is updated, delete line directly below
-    response = requests.get("http://localhost:5001/api/trivia/1")
+    response = requests.get(RANDOM_TRIVIA_FACT_URL)
 
     random_fact = response.json()
 
     # Start our TwiML response
     resp = MessagingResponse()
 
-    # # When database is updated, use this instead to get fact statement
-    # # Determine the right reply for this message
-    # if txt == 'fact':
-    #     resp.message(f"{random_fact['fact']['statement']}")
-    # else:
-    #     resp.message("Please text 'fact' to get a random fact")
-
-    # When database is updated, delete below if/else statement
     if txt == 'fact':
-        resp.message(f"interesting fact for {random_fact['error']['message']}")
+        resp.message(f"{random_fact['fact']['statement']}")
     else:
-        resp.message("Please type Fact to get a random fact")
+        resp.message("Please text 'fact' to get a random fact")
 
     return str(resp)
 
--- nums_api/config.py
+++ nums_api/config.py
@@ -8,3 +8,5 @@ DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_URL_TEST = os.environ['DATABASE_URL_TEST']
 
 MAX_BATCH = 50
+
+RANDOM_TRIVIA_FACT_URL = "http://localhost:5001/api/trivia/random"
