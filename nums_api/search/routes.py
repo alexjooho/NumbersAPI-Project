aff21f7f7d0b53208444b2ac4e1cf614b987c7ea
Ryan Rustyn <ryanrustyn@gmail.com>
2022-10-13 11:42:47 -0700

    add comprehension to results
    comprehension to return db query into jsonable array of POPDs

--- nums_api/search/routes.py
+++ nums_api/search/routes.py
@@ -3,17 +3,24 @@ from nums_api.trivia.models import Trivia
 
 search = Blueprint("search", __name__)
 
+
 @search.get("/")
 def get_search_results():
     """api/search/?query=aliquot"""
-    
+
     args = request.args
     print("search query:", args)
-    
+
     term = args["query"]
     print("search term:", term)
-    
+
     results = Trivia.query.filter(Trivia.__ts_vector__.match(term)).all()
     print("results are:", results)
-    
-    return jsonify(results = "results")
+
+    results = [{"number": r.number,
+                "statement": r.fact_statement,
+                "type": "trivia"
+                }
+               for r in results]
+
+    return jsonify(results=results)
